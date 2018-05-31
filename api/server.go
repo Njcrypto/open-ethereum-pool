6300e317615542add4a6a0dc5ea34e347e7438f5
njcrypto <32847326+Njcrypto@users.noreply.github.com>
2018-05-31 14:58:58 -0400

    Update server.go

--- api/server.go
+++ api/server.go
@@ -12,8 +12,8 @@ import (
 
 	"github.com/gorilla/mux"
 
-	"github.com/sammy007/open-ethereum-pool/storage"
-	"github.com/sammy007/open-ethereum-pool/util"
+	"github.com/njcrypto/open-ethereum-pool/storage"
+	"github.com/njcrypto/open-ethereum-pool/util"
 )
 
 type ApiConfig struct {
